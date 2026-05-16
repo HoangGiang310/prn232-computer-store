98659ca03fc46d6886ecfd66ee10c8ece561bc4c
HoangGiang310 <kenzakishyina@gmail.com>
2026-05-16 15:06:29 +0700

    PRN Project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-950">
+      <Button>
+        Computer Store Admin
+      </Button>
+    </div>
+  )
+}
+
+export default App
