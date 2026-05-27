39227549b49055b5835066cbe1d801d6d507001f
thien-dev <dinhngocthien2004@gmail.com>
2026-05-27 20:44:07 +0700

    save changes

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a6987e2a82694b0975a36ecf7bf9bebad231ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af29a3eb052fa8190f9a3288082339413d89def6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
